d22bf54591f6c7cda6ebb85db5e64a069b44fe6d
Pavel Novichkov <psnovichkov@pnovich.dhcp.lbnl.us>
2012-10-23 23:57:29 -0700

    buildAdaptorProxy() added

--- src/us/kbase/networks/adaptor/modelseed/ModelSEEDAdaptorFactory.java
+++ src/us/kbase/networks/adaptor/modelseed/ModelSEEDAdaptorFactory.java
@@ -1,14 +1,37 @@
 package us.kbase.networks.adaptor.modelseed;
 
+import java.lang.reflect.Proxy;
+
 import us.kbase.networks.adaptor.Adaptor;
 import us.kbase.networks.adaptor.AdaptorException;
 import us.kbase.networks.adaptor.AdaptorFactory;
+import us.kbase.networks.core.AdaptorInvocationHandler;
 
 public class ModelSEEDAdaptorFactory implements AdaptorFactory {
 
 	@Override
 	public Adaptor buildAdaptor() throws AdaptorException {
-		return new ModelSEEDAdaptor();
+		
+//		return new ModelSEEDAdaptor();
+		
+		return  buildAdaptorProxy();
 	}
-
+	
+	
+	private Adaptor buildAdaptorProxy() throws AdaptorException
+	{
+		Adaptor proxyAdaptor = null; 
+			
+	    try {
+	    	ModelSEEDAdaptor adaptor = new ModelSEEDAdaptor();
+	    	AdaptorInvocationHandler invHandler = new AdaptorInvocationHandler(adaptor);
+	    	proxyAdaptor = (Adaptor) Proxy.newProxyInstance(
+	    					ModelSEEDAdaptor.class.getClassLoader(), 
+	    					new Class[]{Adaptor.class}, 
+	    					invHandler); 
+	    } catch (Exception ex) {
+	    	throw new AdaptorException(ex.getMessage(), ex);
+	    } 	
+	    return proxyAdaptor;
+	}
 }
