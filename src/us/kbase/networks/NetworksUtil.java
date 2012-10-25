49bc39d71940e8bb37506876a13c8f971e3f9e2f
Pavel Novichkov <psnovichkov@pnovich.dhcp.lbnl.us>
2012-10-25 00:32:56 -0700

    basic methods to print and visualize datasets and networks

---
+++ src/us/kbase/networks/NetworksUtil.java
@@ -0,0 +1,106 @@
+package us.kbase.networks;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Paint;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import org.apache.commons.collections15.Transformer;
+
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+
+import us.kbase.networks.adaptor.regprecise.RegPreciseAdaptor;
+import us.kbase.networks.core.Dataset;
+import us.kbase.networks.core.Edge;
+import us.kbase.networks.core.Network;
+import us.kbase.networks.core.Node;
+import us.kbase.networks.core.NodeType;
+
+public class NetworksUtil {
+
+	public static void printNetwork(Network network) {
+		System.out.println("//------- Network");
+		System.out.println("Nodes count = " + network.getGraph().getVertexCount());
+		System.out.println("Edges count = " + network.getGraph().getEdgeCount());
+		System.out.println("Network: " + network.getGraph().toString());
+	}
+	
+	public static void printDatasets(String paramName, List<Dataset> datasets)
+	{
+		System.out.println("//------- Datasets");
+		System.out.println("Datasets count (" + paramName + "): = " + datasets.size());
+		for(Dataset dataset: datasets)
+		{
+			System.out.println(dataset.getId() 
+					+ "\t" + dataset.getNetworkType().getName()
+					+ "\t" + dataset.getDatasetSource().getName()
+					+ "\t" + dataset.getName()
+					);
+		}		
+	}	
+	
+	
+	public static void visualizeNetwork(final Graph<Node,Edge> graph)
+	{
+		Layout<Node, String> layout = new CircleLayout(graph);
+		layout.setSize(new Dimension(600,600));
+		BasicVisualizationServer<Node,String> vv =
+			new BasicVisualizationServer<Node,String>(layout);
+		vv.setPreferredSize(new Dimension(650,650));
+		
+		
+		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller()); 
+		vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());		
+		
+		Transformer<Node,Paint> vertexPaint = new Transformer<Node,Paint>() {
+			public Paint transform(Node node) {
+				Color color = Color.black;
+				if(node.getType() == NodeType.GENE )
+				{
+					color = 
+						node.getProperty(RegPreciseAdaptor.NODE_PROPERTY_REGULATOR) != null 
+						? Color.blue: Color.green;
+				}
+				else if(node.getType() == NodeType.CLUSTER)
+				{
+					if(node.getEntityId().contains("regulon"))
+					{
+						color = color.blue;
+					}
+					else if(node.getName().startsWith("complex "))
+					{
+						color = Color.yellow;
+					}
+					else{
+						color = Color.red;	
+					}					
+				}				
+				return color;
+			}
+		};
+		vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);	
+//		vv.getRenderContext().setEdgeFillPaintTransformer(edgePaint);
+		JFrame frame = new JFrame("Simple Graph View"); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		frame.getContentPane().add(vv); 
+		frame.pack();
+		frame.setVisible(true);
+	}	
+	
+	public static void removeDataset(List<Dataset> datasets, String datasetId) {
+		for(Dataset ds: datasets)
+		{
+			if(ds.getId().equals(datasetId))
+			{
+				datasets.remove(ds);
+				break;
+			}
+		}
+	}	
+}
