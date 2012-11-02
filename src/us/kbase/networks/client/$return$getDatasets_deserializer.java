c252ad197d8af81dce00613b44e80f0582281d22
Pavel Novichkov <psnovichkov@pnovich-841.local>
2012-11-02 02:16:32 -0700

    directory renamed to "client"

---
+++ src/us/kbase/networks/client/$return$getDatasets_deserializer.java
@@ -0,0 +1,29 @@
+package us.kbase.networks.client;
+
+import java.io.*;
+import java.util.*;
+import org.codehaus.jackson.map.*;
+import org.codehaus.jackson.map.annotate.*;
+import org.codehaus.jackson.type.*;
+import org.codehaus.jackson.*;
+
+
+public class $return$getDatasets_deserializer extends JsonDeserializer<$return$getDatasets>
+{
+    public $return$getDatasets deserialize(JsonParser p, DeserializationContext ctx)
+	throws IOException, JsonProcessingException
+    {
+	$return$getDatasets res = new $return$getDatasets();
+	if (!p.isExpectedStartArrayToken())
+	{
+		System.out.println("Bad parse in $return$getDatasets_deserializer: " + p.getCurrentToken());
+		return null;
+	}
+	p.nextToken();
+	res.datasetList = p.readValueAs(new TypeReference<List<Dataset>>(){});
+	JsonToken t = p.nextToken();
+//	System.out.println("exit $return$getDatasets_deserializer with token " + t);
+
+	return res;
+    }
+}
