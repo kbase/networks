88d392170a9ab9cee432e733a444370045f16a8f
Shinjae Yoo <sjyoo@bnl.gov>
2012-12-27 13:32:29 -0500

    Network Test to use proper port

--- src/us/kbase/networks/rpc/KBaseNetworkTest.java
+++ src/us/kbase/networks/rpc/KBaseNetworkTest.java
@@ -14,9 +14,9 @@ import us.kbase.networks.rpc.client.Network;
 
 public class KBaseNetworkTest {
 
-	String url = "http://127.0.0.1:8080/KBaseNetworksRPC/networks";
+//	String url = "http://127.0.0.1:8080/KBaseNetworksRPC/networks";
 //	String url = "http://140.221.92.181:7064/KBaseNetworksRPC/networks";
-//	String url = "http://127.0.0.1:7064/KBaseNetworksRPC/networks";
+	String url = "http://127.0.0.1:7064/KBaseNetworksRPC/networks";
 	KBaseNetworks networksAPI;
 		
 	
