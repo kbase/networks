210059b474f33d7c8f2ea9b8035905bb62533423
Pavel Novichkov <psnovichkov@pnovich-982.local>
2013-01-15 03:27:04 -0800

    code was refactored to remove redundancy and clarify logic

--- src/us/kbase/networks/adaptor/JDBCAdaptor/GenericAdaptor.java
+++ src/us/kbase/networks/adaptor/JDBCAdaptor/GenericAdaptor.java
@@ -182,11 +182,17 @@ public class GenericAdaptor extends AbstractAdaptor{
 		if( !hasDataset(dataset.getId())) return null;
 		if(dataset.getProperty(DefaultEdgeTypes) == null ) return null;
 
+		/*PSN
 		String [] edgeTypeStr = dataset.getProperty(DefaultEdgeTypes).split(":");
 		List<EdgeType> edgeTypes = new ArrayList<EdgeType>();
 		for(String etStr : edgeTypeStr) {
 			edgeTypes.add(Enum.valueOf(EdgeType.class, etStr));
 		}
+		*/
+		//PSN start of replacement
+		List<EdgeType> edgeTypes = getEdgeTypes(DefaultEdgeTypes, dataset);
+		//PSN end of replacement
+
 		return buildNetwork(dataset, edgeTypes);
 	}
 
@@ -207,32 +213,50 @@ public class GenericAdaptor extends AbstractAdaptor{
 				IdGenerator.Network.nextId(),
 				"network" + dataset.getId()+".buildNetwork", 
 				graph);
-
 		// Supported EdgeType checking
+
+		/*PSN
 		String [] dsEdgeTypeStr = dataset.getProperty(SupportedEdgeTypes).split(":");
 		List<EdgeType> dsEdgeTypes = new ArrayList<EdgeType>();
 		for(String ets : dsEdgeTypeStr) { dsEdgeTypes.add(Enum.valueOf(EdgeType.class, ets)); }
 		dsEdgeTypes.retainAll(queryEdgeTypes);
+		*/
+		//PSN start of replacement
+		List<EdgeType> dsEdgeTypes = getEdgeTypes(SupportedEdgeTypes, dataset, queryEdgeTypes);
+		//PSN end of replacement
+
 		if(dsEdgeTypes.size() < 1) return null;
 
 		try {
 			String method_name = "buildNetwork";
 			// For each EdgeTypes
 			for(EdgeType et : dsEdgeTypes) {
+				/*PSN
 				// determine Node Type
 				String ndStr [] = et.toString().split("_");
 
 				NodeType nt1 = Enum.valueOf(NodeType.class, ndStr[0]);
 				NodeType nt2 = Enum.valueOf(NodeType.class, ndStr[1]);
-
+				
 				if(dataset.getProperty(Node1_Type_Mapping_Prefix+ndStr[0]) == null || 
 						dataset.getProperty(Node2_Type_Mapping_Prefix+ndStr[1]) == null)
 					throw new AdaptorException("Couldn't find proper EdgeType to Node EntityType Mapping: " + 
 							et.toString());
 				EntityType et1 = Enum.valueOf(EntityType.class, dataset.getProperty(Node1_Type_Mapping_Prefix+ndStr[0]));
 				EntityType et2 = Enum.valueOf(EntityType.class, dataset.getProperty(Node2_Type_Mapping_Prefix+ndStr[1]));			
+				*/
+				
+				//PSN start of replacement
+				assertEdgeTypeMapping(dataset, et);
+				NodeType nt1 = et.nodeType1();
+				NodeType nt2 = et.nodeType2();
+				EntityType et1 = toEntityType(dataset, Node1_Type_Mapping_Prefix, nt1);
+				EntityType et2 = toEntityType(dataset, Node2_Type_Mapping_Prefix, nt2);
+				//PSN end of replacement
 
 				String property_suffix = method_name + "." + et1 + "_" + et2;
+
+				/*PSN
 				PreparedStatement pstmt = ds2pstmts.get(dataset.getId()).sql2pstmt.get(SQL_Statement_Prefix + property_suffix);
 				if(pstmt == null) continue;
 				ResultSet rs;
@@ -244,6 +268,7 @@ public class GenericAdaptor extends AbstractAdaptor{
 					rs = pstmt.executeQuery();
 				}
 
+				
 				if(dataset.getProperty(Resultset_Node1_Index_Prefix + property_suffix) == null || 
 						dataset.getProperty(Resultset_Node2_Index_Prefix + property_suffix) == null) {
 					throw new AdaptorException("SQL Result Index for Node 1 and 2 are not defined"); 
@@ -254,7 +279,16 @@ public class GenericAdaptor extends AbstractAdaptor{
 				if (dataset.getProperty(Resultset_Weight_Index_Prefix + property_suffix) != null) {
 					weightIdx = Integer.parseInt(dataset.getProperty(Resultset_Weight_Index_Prefix + property_suffix));
 				}
+				*/
+				//PSN start of replacement
+				ResultSet rs = getResultSet(dataset, property_suffix);
+				if(rs == null) continue;
 				
+				assertResultSetNodeIndexes(dataset, property_suffix, Resultset_Node1_Index_Prefix, Resultset_Node2_Index_Prefix);
+				int node1Idx  = getNodeIndex1(dataset, property_suffix);
+				int node2Idx  = getNodeIndex2(dataset, property_suffix);
+				int weightIdx = getWeightIndex(dataset, property_suffix);
+				//PSN end of replacement
 
 				Map<String,Node> nodes = new HashMap<String,Node>(); 
 
@@ -297,11 +331,17 @@ public class GenericAdaptor extends AbstractAdaptor{
 		if( !hasDataset(dataset.getId())) return null;
 		if(dataset.getProperty(DefaultEdgeTypes) == null ) return null;
 
+		/*PSN
 		String [] edgeTypeStr = dataset.getProperty(DefaultEdgeTypes).split(":");
 		List<EdgeType> edgeTypes = new ArrayList<EdgeType>();
 		for(String etStr : edgeTypeStr) {
 			edgeTypes.add(Enum.valueOf(EdgeType.class, etStr));
 		}
+		*/
+		//PSN start of replacement
+		List<EdgeType> edgeTypes = getEdgeTypes(DefaultEdgeTypes, dataset);
+		//PSN end of replacement
+
 		return buildFirstNeighborNetwork(dataset, entity, edgeTypes);
 	}
 
@@ -323,10 +363,16 @@ public class GenericAdaptor extends AbstractAdaptor{
 				graph);
 
 		// Supported EdgeType checking
+		/*PSN
 		String [] dsEdgeTypeStr = dataset.getProperty(SupportedEdgeTypes).split(":");
 		List<EdgeType> dsEdgeTypes = new ArrayList<EdgeType>();
 		for(String ets : dsEdgeTypeStr) { dsEdgeTypes.add(Enum.valueOf(EdgeType.class, ets)); }
 		dsEdgeTypes.retainAll(queryEdgeTypes);
+		*/
+		//PSN start of replacement
+		List<EdgeType> dsEdgeTypes = getEdgeTypes(SupportedEdgeTypes, dataset, queryEdgeTypes);
+		//PSN end of replacement
+
 		if(dsEdgeTypes.size() < 1) return null;
 
 		try {
@@ -334,6 +380,7 @@ public class GenericAdaptor extends AbstractAdaptor{
 			// For each EdgeTypes
 			for(EdgeType et : dsEdgeTypes) {
 				// determine Node Type
+				/*
 				String ndStr [] = et.toString().split("_");
 
 				NodeType nt1 = Enum.valueOf(NodeType.class, ndStr[0]);
@@ -345,9 +392,18 @@ public class GenericAdaptor extends AbstractAdaptor{
 							et.toString());
 				EntityType et1 = Enum.valueOf(EntityType.class, dataset.getProperty(Node1_Type_Mapping_Prefix+ndStr[0]));
 				EntityType et2 = Enum.valueOf(EntityType.class, dataset.getProperty(Node2_Type_Mapping_Prefix+ndStr[1]));			
-
+				*/
+				//PSN start of replacement
+				assertEdgeTypeMapping(dataset, et);
+				NodeType nt1 = et.nodeType1();
+				NodeType nt2 = et.nodeType2();
+				EntityType et1 = toEntityType(dataset, Node1_Type_Mapping_Prefix, nt1);
+				EntityType et2 = toEntityType(dataset, Node2_Type_Mapping_Prefix, nt2);
+				//PSN end of replacement
+								
 				String property_suffix = method_name + "." + et1 + "_" + et2 + "." + entity.getType();
 
+				/*PSN
 				PreparedStatement pstmt = ds2pstmts.get(dataset.getId()).sql2pstmt.get(SQL_Statement_Prefix + property_suffix);
 				if(pstmt == null) continue;
 				String [] psIdx = dataset.getProperty(PreparedStatement_BIND_Prefix1 + property_suffix).split(":");
@@ -374,7 +430,7 @@ public class GenericAdaptor extends AbstractAdaptor{
 					}
 					rs = pstmt.executeQuery();
 				}
-
+				
 				if(dataset.getProperty(Resultset_Node1_Index_Prefix + property_suffix) == null || 
 						dataset.getProperty(Resultset_Node2_Index_Prefix + property_suffix) == null) {
 					throw new AdaptorException("SQL Result Index for Node 1 and 2 are not defined"); 
@@ -385,6 +441,17 @@ public class GenericAdaptor extends AbstractAdaptor{
 				if (dataset.getProperty(Resultset_Weight_Index_Prefix + property_suffix) != null) {
 					weightIdx = Integer.parseInt(dataset.getProperty(Resultset_Weight_Index_Prefix + property_suffix));
 				}
+				*/
+				//PSN start of replacement
+				ResultSet rs = getResultSet(dataset, PreparedStatement_BIND_Prefix1, property_suffix, entity);
+				if(rs == null) continue;
+				
+				assertResultSetNodeIndexes(dataset, property_suffix, Resultset_Node1_Index_Prefix, Resultset_Node2_Index_Prefix);
+				int node1Idx  = getNodeIndex1(dataset, property_suffix);
+				int node2Idx  = getNodeIndex2(dataset, property_suffix);
+				int weightIdx = getWeightIndex(dataset, property_suffix);
+				//PSN end of replacement
+				
 				
 				Node node1 = null;
 				Node node2 = null;
@@ -414,20 +481,23 @@ public class GenericAdaptor extends AbstractAdaptor{
 		return network;
 	}
 
-
-
-
 	@Override
 	public Network buildInternalNetwork(Dataset dataset, List<Entity> entities)
 			throws AdaptorException {
 		if( !hasDataset(dataset.getId())) return null;
 		if(dataset.getProperty(DefaultEdgeTypes) == null ) return null;
 
+		/*PSN
 		String [] edgeTypeStr = dataset.getProperty(DefaultEdgeTypes).split(":");
 		List<EdgeType> edgeTypes = new ArrayList<EdgeType>();
 		for(String etStr : edgeTypeStr) {
 			edgeTypes.add(Enum.valueOf(EdgeType.class, etStr));
 		}
+		*/
+		//PSN start of replacement
+		List<EdgeType> edgeTypes = getEdgeTypes(DefaultEdgeTypes, dataset);
+		//PSN end of replacement
+
 		return buildInternalNetwork(dataset, entities, edgeTypes);
 	}
 
@@ -493,12 +563,18 @@ public class GenericAdaptor extends AbstractAdaptor{
 				IdGenerator.Network.nextId(),
 				"network" + dataset.getId()+"."+ qestr1.replaceAll("','", "-") +":" + qestr2.replaceAll("','","-")+".internalNetwork." + qetstr1 +"_"+qetstr2, 
 				graph);
-
 		// Supported EdgeType checking
+
+		/*PSN		
 		String [] dsEdgeTypeStr = dataset.getProperty(SupportedEdgeTypes).split(":");
 		List<EdgeType> dsEdgeTypes = new ArrayList<EdgeType>();
 		for(String ets : dsEdgeTypeStr) { dsEdgeTypes.add(Enum.valueOf(EdgeType.class, ets)); }
 		dsEdgeTypes.retainAll(queryEdgeTypes);
+		 */				
+		//PSN start of replacement
+		List<EdgeType> dsEdgeTypes = getEdgeTypes(SupportedEdgeTypes, dataset, queryEdgeTypes);
+		//PSN end of replacement
+
 		if(dsEdgeTypes.size() < 1) return null;
 
 		try {
@@ -506,6 +582,7 @@ public class GenericAdaptor extends AbstractAdaptor{
 			// For each EdgeTypes
 			for(EdgeType et : dsEdgeTypes) {
 				// determine Node Type
+				/*PSN
 				String ndStr [] = et.toString().split("_");
 
 				NodeType nt1 = Enum.valueOf(NodeType.class, ndStr[0]);
@@ -517,6 +594,14 @@ public class GenericAdaptor extends AbstractAdaptor{
 							et.toString());
 				EntityType et1 = Enum.valueOf(EntityType.class, dataset.getProperty(Node1_Type_Mapping_Prefix+ndStr[0]));
 				EntityType et2 = Enum.valueOf(EntityType.class, dataset.getProperty(Node2_Type_Mapping_Prefix+ndStr[1]));			
+				*/
+				//PSN start of replacement
+				assertEdgeTypeMapping(dataset, et);
+				NodeType nt1 = et.nodeType1();
+				NodeType nt2 = et.nodeType2();
+				EntityType et1 = toEntityType(dataset, Node1_Type_Mapping_Prefix, nt1);
+				EntityType et2 = toEntityType(dataset, Node2_Type_Mapping_Prefix, nt2);
+				//PSN end of replacement
 
 				//if(et1 == et2 && qet2list.size() != 1) {
 				//	throw new AdaptorException(et1.toString() +"_" + et2.toString() + " network but there is two query entity types");
@@ -564,7 +649,13 @@ public class GenericAdaptor extends AbstractAdaptor{
 
 				}
 
+				/*PSN
 				PreparedStatement pstmt = ds2pstmts.get(dataset.getId()).sql2pstmt.get(SQL_Statement_Prefix + property_suffix);
+				*/
+				//PSN start of replacement
+				PreparedStatement pstmt = getPreparedStatement(dataset, property_suffix);
+				//PSN end of replacement
+
 				if (pstmt == null) continue;
 				Connection con = cpds.getConnection();
 				
@@ -599,6 +690,7 @@ public class GenericAdaptor extends AbstractAdaptor{
 				Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery(new_query);
 
+				/*PSN
 				if(dataset.getProperty(Resultset_Node1_Index_Prefix + property_suffix) == null || 
 						dataset.getProperty(Resultset_Node2_Index_Prefix + property_suffix) == null) {
 					throw new AdaptorException("SQL Result Index for Node 1 and 2 are not defined"); 
@@ -609,6 +701,14 @@ public class GenericAdaptor extends AbstractAdaptor{
 				if (dataset.getProperty(Resultset_Weight_Index_Prefix + property_suffix) != null) {
 					weightIdx = Integer.parseInt(dataset.getProperty(Resultset_Weight_Index_Prefix + property_suffix));
 				}
+				*/
+				//PSN start of replacement
+				assertResultSetNodeIndexes(dataset, property_suffix, Resultset_Node1_Index_Prefix, Resultset_Node2_Index_Prefix);
+				int node1Idx  = getNodeIndex1(dataset, property_suffix);
+				int node2Idx  = getNodeIndex2(dataset, property_suffix);
+				int weightIdx = getWeightIndex(dataset, property_suffix);
+				//PSN end of replacement
+				
 
 				Map<String,Node> nodes = new TreeMap<String,Node>(); 
 				while(rs.next()) {
@@ -657,6 +757,7 @@ public class GenericAdaptor extends AbstractAdaptor{
 				return false;
 			}
 
+			/*PSN
 			PreparedStatement pstmt = ds2pstmts.get(dataset.getId()).sql2pstmt.get(SQL_Statement_Prefix + property_suffix);
 			if(pstmt == null) return false;
 			String [] psIdx = dataset.getProperty(PreparedStatement_BIND_Prefix1 + property_suffix).split(":");
@@ -682,7 +783,13 @@ public class GenericAdaptor extends AbstractAdaptor{
 					}
 				}
 				rs = pstmt.executeQuery();
-			}
+			}*/
+			//PSN start of replacement
+			ResultSet rs = getResultSet( dataset, PreparedStatement_BIND_Prefix1, property_suffix,  entity); 			
+			if(rs == null) return false;
+			//PSN end of replacement
+
+			
 			boolean result = false;
 			if(rs.next()) {
 				if(rs.getInt(1) > 0) {
@@ -707,5 +814,120 @@ public class GenericAdaptor extends AbstractAdaptor{
 		}
 		return result;
 	}
+	
+	
+	//PSN methods
+	private void assertResultSetNodeIndexes(Dataset dataset,
+			String propertySuffix, String ... resultsetNodeIndexPrefixes) throws AdaptorException {
+		
+		for(int i = 0; i < resultsetNodeIndexPrefixes.length; i++)
+		{
+			if(dataset.getProperty(resultsetNodeIndexPrefixes[i] + propertySuffix) == null)
+			{
+				throw new AdaptorException("SQL Result Index for Node " + (i + 1) + " are not defined");				
+			}
+		}		
+	}
+	
+	private void assertEdgeTypeMapping(Dataset dataset, EdgeType et) throws AdaptorException {
+		if(dataset.getProperty(Node1_Type_Mapping_Prefix + et.nodeType1().name()) == null || 
+				dataset.getProperty(Node2_Type_Mapping_Prefix + et.nodeType2().name()) == null)
+			throw new AdaptorException("Couldn't find proper EdgeType to Node EntityType Mapping: " + 
+					et.toString());	
+	}	
+	
+	private EntityType toEntityType(Dataset dataset, String nodeTypeMappingPrefix, NodeType nt) {
+		return  Enum.valueOf(EntityType.class, dataset.getProperty(nodeTypeMappingPrefix + nt.name()));
+	}
+	
+	private List<EdgeType> getEdgeTypes(String edgeTypesPrefix, Dataset dataset, List<EdgeType> queryEdgeTypes)
+	{
+		List<EdgeType> dsEdgeTypes = getEdgeTypes(edgeTypesPrefix, dataset);
+		dsEdgeTypes.retainAll(queryEdgeTypes);		
+		return dsEdgeTypes;
+	}
+	
+	private List<EdgeType> getEdgeTypes(String edgeTypesPrefix, Dataset dataset)
+	{
+		String [] dsEdgeTypeStr = dataset.getProperty(edgeTypesPrefix).split(":");
+		List<EdgeType> dsEdgeTypes = new ArrayList<EdgeType>();
+		for(String ets : dsEdgeTypeStr) { 
+			dsEdgeTypes.add(Enum.valueOf(EdgeType.class, ets)); 
+		}
+		return dsEdgeTypes;		
+	}
+	
+	private int getWeightIndex(Dataset dataset, String propertySuffix) {
+		int weightIdx = -1;
+		if (dataset.getProperty(Resultset_Weight_Index_Prefix + propertySuffix) != null) {
+			weightIdx = Integer.parseInt(dataset.getProperty(Resultset_Weight_Index_Prefix + propertySuffix));
+		}
+		return weightIdx;
+	}
 
+	private int getNodeIndex2(Dataset dataset, String propertySuffix) {
+		return Integer.parseInt(dataset.getProperty(Resultset_Node2_Index_Prefix  + propertySuffix));
+	}
+
+	private int getNodeIndex1(Dataset dataset, String propertySuffix) {
+		return Integer.parseInt(dataset.getProperty(Resultset_Node1_Index_Prefix  + propertySuffix));
+	}
+
+	private ResultSet getResultSet(Dataset dataset, String propertySuffix) throws SQLException {
+		
+		PreparedStatement pstmt = getPreparedStatement(dataset,propertySuffix);
+		if(pstmt == null) return null;
+		
+		ResultSet rs;
+		try {
+			rs = pstmt.executeQuery();
+		} catch (SQLException e) {
+			ds2pstmts.get(dataset.getId()).reestablishStatements();
+			pstmt = getPreparedStatement(dataset,propertySuffix);
+			if(pstmt == null) return null;
+			rs = pstmt.executeQuery();
+		}
+
+		return rs;
+	}	
+	
+	
+	private ResultSet getResultSet(Dataset dataset, String preparedStatementPrefix,
+			String propertySuffix, Entity entity) throws NumberFormatException, SQLException {
+		
+		PreparedStatement pstmt = getPreparedStatement(dataset,propertySuffix);
+		if(pstmt == null) return null;
+		setPreparedStatementParameters(dataset,pstmt, preparedStatementPrefix,propertySuffix,entity);
+		
+		ResultSet rs;
+		try {
+			rs = pstmt.executeQuery();
+		} catch (SQLException e) {
+			ds2pstmts.get(dataset.getId()).reestablishStatements();
+			pstmt = getPreparedStatement(dataset,propertySuffix);
+			if(pstmt == null) return null;
+			setPreparedStatementParameters(dataset,pstmt, preparedStatementPrefix,propertySuffix,entity);
+			rs = pstmt.executeQuery();
+		}
+
+		return rs;
+	}
+	
+	private PreparedStatement getPreparedStatement(Dataset dataset,  String propertySuffix) {		
+		return ds2pstmts.get(dataset.getId()).sql2pstmt.get(SQL_Statement_Prefix + propertySuffix);
+	}
+	
+	private void setPreparedStatementParameters(Dataset dataset, PreparedStatement pstmt, String preparedStatementPrefix, String propertySuffix, Entity entity) 
+		throws NumberFormatException, SQLException
+	{
+		String [] psIdx = dataset.getProperty(preparedStatementPrefix + propertySuffix).split(":");
+		for (String pi : psIdx) {
+			if(dataset.getProperty(SQL_Like_Statement_Prefix + propertySuffix) != null) {
+				pstmt.setString(Integer.parseInt(pi), entity.getId() + '%');
+			} else {
+				pstmt.setString(Integer.parseInt(pi), entity.getId());
+			}
+		}		
+	}
+	
 }
