2d359867e124deb9e3d9da2e284adfded0ecd6da
Pavel Novichkov <psnovichkov@pnovich-982.local>
2013-01-15 03:26:20 -0800

    added methods to convert EdgeType to NodeTypes

--- src/us/kbase/networks/core/EdgeType.java
+++ src/us/kbase/networks/core/EdgeType.java
@@ -4,4 +4,19 @@ public enum EdgeType {
 	GENE_GENE, GENE_CLUSTER, 
 	PROTEIN_PROTEIN, PROTEIN_CLUSTER,
 	CLUSTER_CLUSTER;
+	
+	public NodeType nodeType1()
+	{
+		return nodeType(0);
+	}
+	
+	public NodeType nodeType2()
+	{
+		return nodeType(1);
+	}
+	
+	private NodeType nodeType(int index)
+	{
+		return Enum.valueOf(NodeType.class, name().split("_")[index]);
+	}
 }
